9aaa55789f02b6b9c05cb45245f92890a2d9e329
Johann3421 <1355430@senati.pe>
2023-08-17 19:02:35 -0500

    Implemente un for para que me devuelva los numeros ordenados

--- Trabajo2/src/main/java/com/mycompany/trabajo2/Trabajo2.java
+++ Trabajo2/src/main/java/com/mycompany/trabajo2/Trabajo2.java
@@ -13,10 +13,19 @@ public class Trabajo2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] numeros=new int[5];
         System.out.println("Ingrese 5 numeros: ");
         for (int i=0;i<5;i++){
             System.out.println("Numero "+(i+1)+": ");
             numeros[i]=sc.nextInt();
         }
+        ordenarBurbuja(numeros);
+        System.out.println("Numeros ordenados de menor a mayor: ");
+        for (int num : numeros){
+            System.out.println(num+" ");
+        }
+    }
+    public static void ordenarBurbuja(int[]array){
+        
     }
 }
