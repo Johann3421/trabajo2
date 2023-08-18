cea818b9d3419b4f7ce2430c39524911a0a92523
sandpinedo <1440690@senati.pe>
2023-08-17 19:16:12 -0500

    ingrese un sout que pide el ingreso de 5 numeros diversos

--- algoribusqueda/src/main/java/com/mycompany/algoribusqueda/Algoribusqueda.java
+++ algoribusqueda/src/main/java/com/mycompany/algoribusqueda/Algoribusqueda.java
@@ -12,5 +12,9 @@ public class Algoribusqueda {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[]numbers = new int[5];
+        
+        System.out.println("Ingrese 5 numeros: ");
+        
     }
 }
