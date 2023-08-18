421282f1a34bea0347b20d0eb11208a4f9fe69f0
Johann3421 <1355430@senati.pe>
2023-08-17 19:12:29 -0500

    Agregue el sistema final usando dos bucles for para que me devuelva corretamente los numeros

--- Trabajo2/src/main/java/com/mycompany/trabajo2/Trabajo2.java
+++ Trabajo2/src/main/java/com/mycompany/trabajo2/Trabajo2.java
@@ -26,6 +26,16 @@ public class Trabajo2 {
         }
     }
     public static void ordenarBurbuja(int[]array){
+        int n=array.length;
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
         
     }
 }
