a37d993fcdbc89194abd33558aef2e166e8bb3aa
sandpinedo <1440690@senati.pe>
2023-08-17 19:10:28 -0500

    procedi a crear la clase principal y scanner

--- algoribusqueda/src/main/java/com/mycompany/algoribusqueda/Algoribusqueda.java
+++ algoribusqueda/src/main/java/com/mycompany/algoribusqueda/Algoribusqueda.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.algoribusqueda;
-
+import java.util.Scanner;
 /**
  *
  * @author Alumno
@@ -11,6 +11,6 @@ package com.mycompany.algoribusqueda;
 public class Algoribusqueda {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
     }
 }
