b37ea3c6cfca253d46ec4796dbaafa37f6461da2
sandpinedo <1440690@senati.pe>
2023-08-17 19:05:58 -0500

    sandro

---
+++ algoribusqueda/src/main/java/com/mycompany/algoribusqueda/Algoribusqueda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoribusqueda;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Algoribusqueda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
